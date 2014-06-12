f6ac386960d33dbba688aed20ed4c1af46b51eaa
aidma06 <aidma06@aidma-training>
2014-06-12 21:16:37 +0900

    6/12

---
+++ Chap10/src/main/java/action/IndexAction.java
@@ -0,0 +1,15 @@
+package action;
+
+public class IndexAction {
+	private String message;
+	public void setMessage(String message){
+		this.message = message;
+	}
+	public String getMessage(){
+		return message;
+	}
+	public String execute(){
+		setMessage("Hello");
+		return "success";
+	}
+}
